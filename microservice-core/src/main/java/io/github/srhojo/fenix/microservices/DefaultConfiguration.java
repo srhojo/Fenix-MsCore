cd49297e05fa595104c8ef7d25ac26358a526d88
srhojo <j.lacalle@icloud.com>
2019-07-28 21:31:25 +0200

    Add config

--- microservice-core/src/main/java/io/github/srhojo/fenix/microservices/DefaultConfiguration.java
+++ microservice-core/src/main/java/io/github/srhojo/fenix/microservices/DefaultConfiguration.java
@@ -4,7 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.base.Predicates;
 
 import springfox.documentation.builders.PathSelectors;
@@ -28,5 +34,33 @@ public class DefaultConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
                 .paths(Predicates.not(PathSelectors.regex("/error.*"))).build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        return mapper;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedHeaders("Authorization", "Cache-Control", "Content-Type", "Accept", "X-Requested-With",
+                                "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
+                        .exposedHeaders("Access-Control-Expose-Headers", "Authorization", "Cache-Control",
+                                "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
+                                "Origin");
+
+            }
+        };
     }
 }
